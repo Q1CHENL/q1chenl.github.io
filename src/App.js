fe64394e8dd90750de8608615f84dc4e88cd25a8
Qichen Liu <liuqichne@gmail.com>
2025-05-05 16:19:38 +0200

    Remove React logo. Update X.com icon.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import { FaGithub, FaTwitter } from 'react-icons/fa';
-import logo from './logo.svg';
+import { FaGithub } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi👋 I'm Qichen Liu (刘启辰)
+          Hi👋 I'm Qichen Liu (刘启辰).
         </p>
         <div className="social-links">
           <a href="https://github.com/q1chenl" target="_blank" rel="noopener noreferrer">
             <FaGithub size={30} />
           </a>
           <a href="https://x.com/qichen404" target="_blank" rel="noopener noreferrer">
-            <FaTwitter size={30} />
+            <FaXTwitter size={30} />
           </a>
         </div>
       </header>
